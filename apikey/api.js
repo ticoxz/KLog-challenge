0c669c99f9d43c0797352260a61c5f92c91d79c3
Marcelo Miranda <ticomiranda4@gmail.com>
2021-09-24 10:59:27 -0300

    hidecredentials

--- apikey/api.js
+++ apikey/api.js
@@ -4,7 +4,7 @@ const API_KEY = process.env.NEXT_PUBLIC_CLIENT_ID;
 
 export const getQueryPhotos = async (query) => {
     console.log(query)
-    const res = await fetch(`https://api.unsplash.com/search/photos?page=1&query=${query}&per_page=15&client_id=3H516dDIPOgaJaMNiimPnCh9dh1e4GAYawjiD1sOByo`)
+    const res = await fetch(`https://api.unsplash.com/search/photos?page=1&query=${query}&per_page=15&client_id=${process.env.NEXT_PUBLIC_CLIENT_ID}`)
     const responseJson = await res.json();
     return responseJson.results;
 };
